e113e3ac1eee8476b94a3facbe4ef524cbce0b16
Anjasfedo <fedoaf98@gmail.com>
2024-02-12 23:42:51 +0700

    CTA Component

---
+++ src/components/Cta.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<div class="flex items-center gap-4">
+  <button class={"specialButton"}>
+    <p class="text-base sm:text-lg md:text-xl">Contact Use</p>
+  </button>
+  <button class={"specialButtonDark"}>
+    <p class="text-base sm:text-lg md:text-xl">Get Started &rarr;</p>
+  </button>
+</div>
