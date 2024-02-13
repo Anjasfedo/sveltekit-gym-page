3a98e70d043ef4e9d1897833f8ca22726bbc1a6d
Anjasfedo <fedoaf98@gmail.com>
2024-02-13 20:24:41 +0700

    Faq Component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,12 @@
   import Hero from "../components/Hero.svelte";
   import Product from "../components/Product.svelte";
   import Review from "../components/Review.svelte";
+  import Faq from "../components/Faq.svelte"
 </script>
 
 <main class="flex flex-col">
   <Hero />
   <Product />
   <Review />
+  <Faq />
 </main>
